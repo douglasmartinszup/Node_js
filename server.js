e351235753cc78865decfa53d3ff57afecdd4ccb
douglasmartinszup <douglas.martins@zup.com.br>
2020-11-15 18:16:26 -0300

    configuraçao de Cors, deixei api publica

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
-
+const cors = require('cors');
 //Iniciando o App
 const app = express();
 app.use(express.json());

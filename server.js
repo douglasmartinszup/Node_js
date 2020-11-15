d38073490d0da7e4a23bbd5edce354d6e28f4a26
douglasmartinszup <douglas.martins@zup.com.br>
2020-11-15 17:45:24 -0300

    criaçao de registros

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const requireDir = require("require-dir");
 
 //Iniciando o App
 const app = express();
-
+app.use(express.json());
 //Iniciando do DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,

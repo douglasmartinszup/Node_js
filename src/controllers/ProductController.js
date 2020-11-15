2cfc3f6c2194a95c1de2001b2704922a280ef39d
douglasmartinszup <douglas.martins@zup.com.br>
2020-11-15 18:03:12 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -7,10 +7,33 @@ module.exports = {
     const products = await Product.find();
     return res.json(products);
   },
-
-  async store(req,res) {
-      //Criação
-      const product = await Product.create(req.body);
-      return res.json(product);
+  //Rota de detalhe
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
   },
+
+  //Rota de post
+  async store(req, res) {
+    //Criação
+    const product = await Product.create(req.body);
+    return res.json(product);
+  },
+
+
+    //Rota de atualização
+  async update(req,res) {
+      const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+  return res.json(product);
+  
+    },
+
+
+
+  //Rota de Delete
+  async destroy(req, res) {
+      await Product.findByIdAndRemove(req.params.id);
+      return res.send();
+  },
+
 };

3795479f634ae00de55717fe630b611cb88e8d02
douglasmartinszup <douglas.martins@zup.com.br>
2020-11-15 15:12:41 -0300

    Criando a primeira rota

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 //criamos uma const app
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send("by Doug Caval");
+});
 //pedimos para ela "ouvir " a porta 3001
 app.listen(3001);
